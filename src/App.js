37fe20526318e5aa65d6cc6b65d1cbc2a9f93b96
arshad <arshad@gmail.com>
2023-08-25 09:43:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="grid place-items-center h-screen" >
+    <div className="bg-blue-500 text-white p-4 flex flex-col items-center">
+    
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+      <p   className="mt-2" >This is Paragraph</p>
+    </div>
+    <div class="flex flex-row">
+  <div class="bg-red-300 p-2">First Item</div>
+  <div class="bg-blue-300 p-2">Second Item</div>
+  <div class="bg-green-300 p-2">Third Item</div>
+</div>
+
     </div>
   );
 }
